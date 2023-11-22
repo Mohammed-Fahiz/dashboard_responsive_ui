6e78ebe98f297e95fd65c090b7eda4230e16b7f0
Mohammed-Fahiz <mohammedfahiz73@gmail.com>
2023-11-22 23:54:04 +0530

    finished

---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:responsive_app_livestream/util/responsive.dart';
+
+import '../controller/controller.dart';
+import '../widgets/dashboard/dashboard_screen.dart';
+import '../widgets/side_menu_widget.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: context.read<MenuAppController>().scaffoldKey,
+      // drawer: SideMenuWidget(),
+      body: SafeArea(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // We want this side menu only for large screen
+            if (Responsive.isDesktop(context))
+              const Expanded(
+                // default flex = 1
+                // and it takes 1/6 part of the screen
+                child: SideMenuWidget(),
+              ),
+            const Expanded(
+              // It takes 5/6 part of the screen
+              flex: 5,
+              child: DashboardScreen(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/dashboard/dashboard_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_app_livestream/util/responsive.dart';
+
+import '../../constant/constants.dart';
+import 'components/header.dart';
+import 'components/my_files.dart';
+import 'components/recent_files.dart';
+import 'components/storage_details.dart';
+
+class DashboardScreen extends StatelessWidget {
+  const DashboardScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SingleChildScrollView(
+        primary: false,
+        padding: const EdgeInsets.all(defaultPadding),
+        child: Column(
+          children: [
+            const Header(),
+            const SizedBox(height: defaultPadding),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  flex: 5,
+                  child: Column(
+                    children: [
+                      const MyFiles(),
+                      const SizedBox(height: defaultPadding),
+                      const RecentFiles(),
+                      if (Responsive.isMobile(context)) const StarageDetails(),
+                    ],
+                  ),
+                ),
+                const SizedBox(width: defaultPadding),
+                // On Mobile we don't want to show it
+                if (!Responsive.isMobile(context))
+                  const Expanded(
+                    flex: 2,
+                    child: StarageDetails(),
+                  ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
